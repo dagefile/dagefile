67c27929dbe9eb53b8bf4cd2b2fb406025e51bd6
ithreewm <ithreewm@proton.me>
2026-09-08 23:58:22 +0000

    Update i3-install.sh

--- github/desktop-lite/local-features/da-desktop/i3-install.sh
+++ github/desktop-lite/local-features/da-desktop/i3-install.sh
@@ -232,7 +232,7 @@ fi
 
 # Run custom startup script in background
 if [ -f "/usr/local/share/dagithubinit.sh" ]; then
-    sudoUserIf "/usr/local/share/dagithubinit.sh"
+    sudoIf "/usr/local/share/dagithubinit.sh"
     log "dagithubinit started."
 fi
 
